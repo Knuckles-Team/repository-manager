6e403ccfae0699134d3a0ee36e87e63e891b4dde
Audel Rouhi <knucklessg1@gmail.com>
2025-10-03 01:26:38 -0500

    Import all funcs in init.

--- repository_manager/__init__.py
+++ repository_manager/__init__.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
-from repository_manager.repository_manager import Git, repository_manager, setup_logging
-from repository_manager.repository_manager_mcp import repository_manager_mcp
+
+import importlib
+import inspect
+
+# List of modules to import from
+MODULES = [
+    "repository_manager.repository_manager",
+    "repository_manager.repository_manager_mcp",
+]
+
+# Initialize __all__ to expose all public classes and functions
+__all__ = []
+
+# Dynamically import all classes and functions from the specified modules
+for module_name in MODULES:
+    module = importlib.import_module(module_name)
+    for name, obj in inspect.getmembers(module):
+        # Include only classes and functions, excluding private (starting with '_')
+        if (inspect.isclass(obj) or inspect.isfunction(obj)) and not name.startswith(
+            "_"
+        ):
+            globals()[name] = obj
+            __all__.append(name)
 
 """
 report-manager
 
 Manage your git projects
 """
-
-
-__all__ = ["Git", "repository_manager", "setup_logging", "repository_manager_mcp"]
