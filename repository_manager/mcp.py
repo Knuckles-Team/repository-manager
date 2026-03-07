4309565dbe9ea421ff99442762c2cda6c3c74f45
KnucklesSG1 <knucklessg1@gmail.com>
2026-03-07 02:27:18 -0600

    Bump version: 1.3.28 → 1.3.29

--- repository_manager/mcp.py
+++ repository_manager/mcp.py
@@ -5,7 +5,7 @@ from agent_utilities.base_utilities import to_boolean
 import os
 import sys
 
-__version__ = "1.3.28"
+__version__ = "1.3.29"
 
 from typing import Optional, Dict, List, Any
 from pydantic import Field
--- repository_manager/repository_manager.py
+++ repository_manager/repository_manager.py
@@ -13,7 +13,7 @@ import sys
 import argparse
 import logging
 
-__version__ = "1.3.28"
+__version__ = "1.3.29"
 import concurrent.futures
 import datetime
 from typing import List
